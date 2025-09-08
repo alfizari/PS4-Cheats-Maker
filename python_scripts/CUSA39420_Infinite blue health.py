22c1c46f44ac0f4d79b2b823e1999c78a80a6734
Ahmed <166356542+alfizari@users.noreply.github.com>
2025-09-08 13:37:29 -0500

    Update CUSA39420_Infinite blue health.py

--- python_scripts/CUSA39420_Infinite blue health.py
+++ python_scripts/CUSA39420_Infinite blue health.py
@@ -4,5 +4,6 @@
 offset=search_first_text_simple('healthBlue', save_data, encoding='utf-8')
 health_off=offset+12
 
-insert_bytes(health_off, '39 39 39 39', save_data)
+insert_bytes(health_off, '39 39 39', save_data)
+
 
